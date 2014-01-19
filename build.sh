a10d10cba1a5ee27a618b3798ffa82f021920b8e
Andrea Zonca <code@andreazonca.com>
2014-01-18 22:58:31 -0800

    change name for content folder

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 REPOSITORY_NAME=$1
-git clone --depth 1 https://github.com/openproceedings/$REPOSITORY_NAME.git
+git clone --depth 1 https://github.com/openproceedings/$REPOSITORY_NAME.git content
 make html
