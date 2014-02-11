ea29fda3ba09a5526063b9b8463b36062b1a06d0
Andrea Zonca <code@andreazonca.com>
2014-02-10 19:23:38 -0800

    Commit changes to the content repository

--- build.sh
+++ build.sh
@@ -6,3 +6,15 @@ git clone --quiet https://${GH_TOKEN_CONTENT}@github.com/$REPOSITORY_NAME.git co
 ln -s content/images images
 ls images/
 make html
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    if [ "$TRAVIS" == "true" ]; then
+        git config --global user.email "travis@travis-ci.org"
+        git config --global user.name "Travis"
+    fi
+    # commit json information to the repository
+    cd content
+    git add -f .
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER: figshare metadata" -m "[ci skip]"
+    git push -fq origin master &> /dev/null
+    echo -e "Figshare metadata added to content repository\n"
+fi
--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     #add, commit and push files
     git add -f .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git push -fq origin $BRANCH > /dev/null
+    git push -fq origin $BRANCH &> /dev/null
     echo -e "Deploy completed\n"
 fi
